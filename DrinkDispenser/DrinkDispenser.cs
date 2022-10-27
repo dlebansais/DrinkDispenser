9080132e4e39a00f5b524a1d74c78bf7b1920552
dlebansais <dlebansais@gmail.com>
2022-10-27 08:43:44 +0200

    Add project files.

---
+++ DrinkDispenser/DrinkDispenser.cs
@@ -0,0 +1,9 @@
+﻿namespace DrinkDispenser;
+
+public class DrinkDispenser
+{
+    public static double GetDrinkPrice(string recipeName)
+    {
+        return 0;
+    }
+}
---
+++ Test/Test-DrinkDispenser/TestDrinkDispenser.cs
@@ -0,0 +1,15 @@
+﻿namespace Test_DrinkDispenser;
+
+using NUnit;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestDrinkDispenser
+{
+    [Test]
+    public void TestBasic()
+    {
+        double Price = DrinkDispenser.DrinkDispenser.GetDrinkPrice("Expresso");
+        Assert.That(Price, Is.EqualTo(1.2 * 1.3));
+    }
+}
